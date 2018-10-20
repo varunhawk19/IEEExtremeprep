765792160af80ecbdcabc436afaf50e8bb24ef8d
varunhawk19 <varunhawk19@gmail.com>
2018-10-20 21:35:28 +0530

    Update god_why.cpp

--- god_why.cpp
+++ god_why.cpp
@@ -1,125 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool func(){
+	register int a,b,temp;
+	bool flag=true;
+	cin>>a>>b;
+	vector<int> v;
+	for(register int i=0;i<b;i++){
+		cin>>temp;
+		v.push_back(temp);
+	}
+	for(register int i=0;i<b;i++){
+		for(register int j=i;j<b;j++){
+			if(v.at(i)+v.at(j)==a){
+				cout<<v.at(i)<<" "<<v.at(j)<<endl;
+				return true;
+			}
+		}
+	}
+	return false;
 
-
-int main(int argc, char const *argv[]){
-	register int t;
+}
+int main(){
+	int t;
 	cin>>t;
 	while(t--){
-		register int a,b,temp;
-		string ans;
-		cin>>a>>b;
-		register bool flag=true;
-		vector<int> v,t;
-		for(register int i=0;i<b;i++){
-			cin>>temp;
-			v.push_back(temp);
+		bool truth=func();
+		if(!func){
+			cout<<"!OK"<<endl;
 		}
-		vector<int>u(v);
-		sort(u.begin(),u.end());
-		for(register int i=0;i<u.size();i++){
-			if(binary_search(u.begin(),u.end(),(a-u.at(i)))){
-				t.push_back(u.at(i));
-			}
-		}#include<bits/stdc++.h>
-	using namespace std;
-
-
-	int main(int argc, char const *argv[]){
-		register int t;
-		cin>>t;
-		while(t--){
-			register int a,b,temp;
-			string ans;
-			cin>>a>>b;
-			register bool flag=true;
-			vector<int> v,t;
-			for(register int i=0;i<b;i++){
-				cin>>temp;
-				v.push_back(temp);
-			}
-			vector<int>u(v);
-			sort(u.begin(),u.end());
-			for(register int i=0;i<u.size();i++){
-				if(binary_search(u.begin(),u.end(),(a-u.at(i)))){
-					t.push_back(u.at(i));
-				}
-			}
-			if(flag){
-				cout<<"!OK";
-			}
-			else{
-
-
-			for(int i=0;i<=b;i++){
-				for(int j=0;j<ans.length();j++)
-					if(t.at(j)==v.at(i)){
-						cout<<v.at(i)<<" "<<b-v.at(i);
-					}
-				
-			}
-
-			cout<<endl;
-		}
-		return 0;
-	} 
-		if(flag){
-			cout<<"!OK";
-		}
-		else{
-
-
-		for(int i=0;i<=b;i++){
-			for(int j=0;j<ans.length();j++)
-				if(t.at(j)==v.at(i)){
-					cout<<v.at(i)<<" "<<b-v.at(i);
-				}
-			
-		}
-		
-		cout<<endl;
 	}
 	return 0;
 }
-#include<bits/stdc++.h>
-using namespace std;
-
-
-int main(int argc, char const *argv[]){
-	register int t;
-	cin>>t;
-	while(t--){
-		register int a,b,temp;
-		string ans;
-		cin>>a>>b;
-		register bool flag=true;
-		vector<int> v,t;
-		for(register int i=0;i<b;i++){
-			cin>>temp;
-			v.push_back(temp);
-		}
-		vector<int>u(v);
-		sort(u.begin(),u.end());
-		for(register int i=0;i<u.size();i++){
-			if(binary_search(u.begin(),u.end(),(a-u.at(i)))){
-				t.push_back(u.at(i));
-			}
-		}
-		if(flag){
-			cout<<"!OK";
-		}
-		else{
-
-
-		for(int i=0;i<=b;i++){
-			for(int j=0;j<ans.length();j++)
-				if(t.at(j)==v.at(i)){
-					cout<<v.at(i)<<" "<<b-v.at(i);
-				}
-			
-		}
-
-		cout<<endl;
-	}
-	return 0;
-} 

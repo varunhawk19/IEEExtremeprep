d81e2e6809a918e568f712d412e042a10399924a
varunhawk19 <varunhawk19@gmail.com>
2018-10-20 21:31:34 +0530

    Update god_why.cpp

--- god_why.cpp
+++ god_why.cpp
@@ -21,7 +21,48 @@ int main(int argc, char const *argv[]){
 			if(binary_search(u.begin(),u.end(),(a-u.at(i)))){
 				t.push_back(u.at(i));
 			}
+		}#include<bits/stdc++.h>
+	using namespace std;
+
+
+	int main(int argc, char const *argv[]){
+		register int t;
+		cin>>t;
+		while(t--){
+			register int a,b,temp;
+			string ans;
+			cin>>a>>b;
+			register bool flag=true;
+			vector<int> v,t;
+			for(register int i=0;i<b;i++){
+				cin>>temp;
+				v.push_back(temp);
+			}
+			vector<int>u(v);
+			sort(u.begin(),u.end());
+			for(register int i=0;i<u.size();i++){
+				if(binary_search(u.begin(),u.end(),(a-u.at(i)))){
+					t.push_back(u.at(i));
+				}
+			}
+			if(flag){
+				cout<<"!OK";
+			}
+			else{
+
+
+			for(int i=0;i<=b;i++){
+				for(int j=0;j<ans.length();j++)
+					if(t.at(j)==v.at(i)){
+						cout<<v.at(i)<<" "<<b-v.at(i);
+					}
+				
+			}
+
+			cout<<endl;
 		}
+		return 0;
+	} 
 		if(flag){
 			cout<<"!OK";
 		}
@@ -40,3 +81,45 @@ int main(int argc, char const *argv[]){
 	}
 	return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[]){
+	register int t;
+	cin>>t;
+	while(t--){
+		register int a,b,temp;
+		string ans;
+		cin>>a>>b;
+		register bool flag=true;
+		vector<int> v,t;
+		for(register int i=0;i<b;i++){
+			cin>>temp;
+			v.push_back(temp);
+		}
+		vector<int>u(v);
+		sort(u.begin(),u.end());
+		for(register int i=0;i<u.size();i++){
+			if(binary_search(u.begin(),u.end(),(a-u.at(i)))){
+				t.push_back(u.at(i));
+			}
+		}
+		if(flag){
+			cout<<"!OK";
+		}
+		else{
+
+
+		for(int i=0;i<=b;i++){
+			for(int j=0;j<ans.length();j++)
+				if(t.at(j)==v.at(i)){
+					cout<<v.at(i)<<" "<<b-v.at(i);
+				}
+			
+		}
+
+		cout<<endl;
+	}
+	return 0;
+} 

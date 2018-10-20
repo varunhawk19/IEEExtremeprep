314cabdacf9d688851b56e423f4af71c6a00c823
varunhawk19 <varunhawk19@gmail.com>
2018-10-20 09:23:26 +0530

    Update intial_temp.cpp

--- intial_temp.cpp
+++ intial_temp.cpp
@@ -1,25 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-struct Node{
-	string parent,child;
-	long long int below,below_direct;
-
-}tem;
-vector <Node> data;
-int main(){
-	long long int n,q;
-	cin>>n>>q;
-	for(int i=0;i<n-1;i++){
-		Node temp;
-		cin>>temp.parent;
-		cin>>temp.child;
-		cin>>temp.below;
-		cin>>temp.below_direct;
-		data.push_back(temp);
-	}
-	for(int i=0;i<data.size();i++){
-		tem=data.at(i);
-		cout<<tem.parent<<" "<<tem.child<<" "<<tem.below<<" "<<tem.below_direct<<endl;
-	}
-	return 0;
-}
+10 3
+company NONE 10 100
+engineering company 5 30
+team1 engineering 10 10
+team2 engineering 0 0
+team2part1 team2 5 5
+team2part2 team2 3 3
+manufacturing company 40 40
+finance company 0 0
+accounting finance 0 0
+sales finance 0 5
+company 1
+team2 2
+accounting 1
